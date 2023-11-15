49ca5fa6a79975e6e9ff51ff575f2bf20bf4aa3b
LollyBomb-hub <34553831+LollyBomb-hub@users.noreply.github.com>
2023-11-15 18:20:44 +0300

    Add files via upload

--- serve/visorserv.py
+++ serve/visorserv.py
@@ -1,60 +1,67 @@
 from fastapi import FastAPI, Request
 
 from sqlalchemy import create_engine
-from sqlalchemy.engine import URL
-from sqlalchemy import Column, Integer, String, DateTime, Text, LargeBinary
-from sqlalchemy.orm import declarative_base
-from datetime import datetime
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import Integer, String, Column, create_engine, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+
+import uuid
+
+
+Base = declarative_base()
+
+class ActivityEntry(Base):
+    __tablename__ = "activity_entry"
+
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    domain = Column(String, index=True)
+    computername = Column(String, index=True)
+    username = Column(String, index=True)
+    hostname = Column(String, index=True)
 
 
 # Простенький сервер
 
 app = FastAPI()
 
-url = URL.create(
-    drivername="drivername",
-    username="username",
-    host="host",
-    database="dbname"
-)
+DBURL = "postgresql://postgres:password@localhost:25432/test"
 
-engine = create_engine(url)
-
-Base = declarative_base()
-
-class Client(Base):
-    __tablename__ = 'clients'
-
-    id = Column(Integer(), primary_key=True)
-    host = Column(String(), index=True)
-    domain = Column(String(), index=True)
-    machine = Column(String(), index=True)
-    username = Column(String(), index=True)
-    photo = Column(LargeBinary())
-    datetime = Column(DateTime(), default=datetime.now)
+engine = create_engine(DBURL, echo=True)
+sess = Session(engine)
 
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
 
 @app.route("/{full_path:path}")
 def capture_routes(request: Request, full_path: str):
     print(request.client.host, request.body(), full_path)
 
 @app.post("/ua")
-def write_to_db(request: Request):
+async def write_to_db(request: Request):
     client_host = request.client.host
-    body = str(request.body())
-    v = body.split('\n')
-    client = Client(
-        host=str(client_host),
-        domain=v[0],
-        machine=v[1],
-        username=v[2],
-        photo=v[3]
-    )
-    session.add(client)
-    session.commit()
-    print(body, client_host)
+    body = await request.body()
+    print(len(body))
+    u = uuid.uuid4()
+    a=0
+    for _ in range(3):
+        a = body.index(b'\n', a + 1)
+    f = body[a + 1:]
+    r = body[:a].split(b'\n')
+    r[0] = r[0].decode("ascii").replace("\x00", "")
+    r[1] = r[1].decode("ascii").replace("\x00", "")
+    r[2] = r[2].decode("ascii").replace("\x00", "")
+    ae = ActivityEntry()
+    ae.id = u
+    ae.domain = r[0]
+    ae.computername = r[1]
+    ae.username = r[2]
+    ae.hostname = client_host
+    sess.add(ae)
+    sess.commit()
+    try:
+        # Предлагается использовать garbage collector для старых фоток. В тз не входит
+        with open(f"{u}.png", 'wb') as screenshot:
+            screenshot.write(f)
+    except:
+        pass
